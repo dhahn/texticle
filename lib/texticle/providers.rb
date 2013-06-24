192b7b27eed53e6956f92aeddb513a939b959132
David Hahn <dhahn@ctatechs.com>
2013-06-24 14:55:08 -0500

    Add file to update providers in. This will probably need to be a yml file sometime.

---
+++ lib/texticle/providers.rb
@@ -0,0 +1,12 @@
+module Texticle
+  PROVIDERS = [
+   '@txt.att.net',
+   '@sms.alltelwireless.com',
+   '@messaging.nextel.com' ,
+   '@messaging.sprintpcs.com',
+   '@tmomail.net',
+   '@email.uscc.net',
+   '@vtext.com' ,
+   '@vmobl.com' 
+  ]
+end
