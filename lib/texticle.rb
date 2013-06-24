9104242aaf12cff601bc6abf1ff317f821eb922b
David Hahn <dhahn@ctatechs.com>
2013-06-24 14:54:38 -0500

    Add text method and private methods for Texticle module

--- lib/texticle.rb
+++ lib/texticle.rb
@@ -1,5 +1,22 @@
-require "texticle/version"
+lib = File.expand_path('../lib', __FILE__)
+$LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
+require 'texticle/version'
+require "texticle/providers"
+require "pony"
 
 module Texticle
-  # Your code goes here...
+  def self.text(options)
+    options[:to] = recipients(options[:to])
+    Pony.mail(options)
+  end
+  
+  private
+    def self.recipients(numbers)
+      recipients = []
+      numbers = numbers.split(",") unless numbers.class == Array
+      numbers.each do |number|
+        PROVIDERS.each { |provider| recipients << "#{number}#{provider}" }
+      end
+      recipients
+    end
 end
