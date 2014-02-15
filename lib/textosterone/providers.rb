42e844f233d87312ef005d72b9ef15fb37e7a20f
David Hahn <dhahn@ctatechs.com>
2014-02-15 13:58:00 -0600

    Update list of providers

--- lib/textosterone/providers.rb
+++ lib/textosterone/providers.rb
@@ -1,12 +1,23 @@
 module Textosterone
+U.S. Cellular: number@email.uscc.net
   PROVIDERS = [
-   '@txt.att.net',
-   '@sms.alltelwireless.com',
-   '@messaging.nextel.com' ,
-   '@messaging.sprintpcs.com',
-   '@tmomail.net',
-   '@email.uscc.net',
-   '@vtext.com' ,
-   '@vmobl.com' 
+    "@txt.att.net",
+    "@sms.alltelwireless.com",
+    "@messaging.nextel.com" ,
+    "@messaging.sprintpcs.com",
+    "@tmomail.net",
+    "@email.uscc.net",
+    "@vtext.com" ,
+    "@mmst5.tracfone.com",
+    "@mymetropcs.com",
+    "@myboostmobile.com",
+    "@sms.mycricket.com",
+    "@messaging.nextel.com",
+    "@message.alltel.com",
+    "@ptel.com",
+    "@tms.suncom.com",
+    "@qwestmp.com",
+    "@vmobl.com" 
   ]
+
 end
